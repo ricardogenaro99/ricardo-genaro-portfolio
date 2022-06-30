b92498676d73aa11f796953af55c33d56ddec326
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-06-30 04:07:48 -0500

    fix: limpiar importaciones

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Main } from "./app/components/main";
 import { Footer, Header } from "./app/shared/components";
